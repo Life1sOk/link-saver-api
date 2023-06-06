80ff0b55466b6700d9df861c2f53f533d0e40902
Life1sOk <mr.lifeisok@gmail.com>
2023-06-06 19:37:16 +0600

    check

--- helpers/connect.js
+++ helpers/connect.js
@@ -5,11 +5,14 @@ const eventEmitter = new EventEmitter();
 const handlerGetConnection = (db) => (req, res) => {
   const { user_id } = req.params;
 
+  res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
   res.writeHead(200, {
     Connection: "keep-alive",
     "Content-type": "text/event-stream",
     "Cache-Control": "no-cache",
-    "Access-Control-Allow-Origin": "*",
+    // "Access-Control-Allow-Origin": "*",
     // 'Access-Control-Allow-Credentials': 'true'
   });
 
